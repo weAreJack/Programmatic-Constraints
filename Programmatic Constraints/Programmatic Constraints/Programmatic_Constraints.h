157bffc3880e089483b598471b4495e7e88e5c68
Jack Smith <jack@wearejurni.com>
2020-05-24 19:22:04 +0100

    Initial commit

---
+++ Programmatic Constraints/Programmatic Constraints/Programmatic_Constraints.h
@@ -0,0 +1,19 @@
+//
+//  Programmatic_Constraints.h
+//  Programmatic Constraints
+//
+//  Created by Jack Smith on 24/05/2020.
+//  Copyright © 2020 WeAreJack. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Programmatic_Constraints.
+FOUNDATION_EXPORT double Programmatic_ConstraintsVersionNumber;
+
+//! Project version string for Programmatic_Constraints.
+FOUNDATION_EXPORT const unsigned char Programmatic_ConstraintsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Programmatic_Constraints/PublicHeader.h>
+
+
